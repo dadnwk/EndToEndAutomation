902a375642889ee4df374ecbf3dd22c472648f25
dadnwk <dadnwk@gmail.com>
2021-04-01 15:06:07 +0700

    Update TC001

--- TestCases/test_TC_001_Search_ValidData.py
+++ TestCases/test_TC_001_Search_ValidData.py
@@ -17,11 +17,3 @@ def test_search_valid_data(data):
     search.enter_key(data[0])
     driver.find_element_by_xpath(ConfigReader.read_elements('Search', 'search_button')).click()
     driver.close()
-
-def test_search_invalid_data(data):
-    driver = InitiateDriver.start_browser()
-    search = SearchPage.SearchPage(driver)
-    driver.find_element_by_xpath(ConfigReader.read_elements('Search', 'search_icon')).click()
-    search.enter_key(data[0])
-    driver.find_element_by_xpath(ConfigReader.read_elements('Search', 'search_button')).click()
-    driver.close()
